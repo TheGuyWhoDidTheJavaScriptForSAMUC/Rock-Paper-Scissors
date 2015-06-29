c9128f1801a47a8ddcc3dcf65fb0a1c01ce903b3
Student 10AM <student10@js-c02mxpvdg083.punahou.edu>
2015-06-29 13:45:34 -1000

    First codes

---
+++ Rock-Paper-Scissors.js
@@ -0,0 +1,13 @@
+function computerThink() {
+	computerChoice = Math.floor(Math.random()*1);
+	if(computerChoice ===1) {
+		computerChoice = "rock";
+	}
+	else if(computerChoice === 0.5){
+		computerChoice = "paper";
+	}
+	else{
+		computerChoice = "scissors";
+	}
+};
+
