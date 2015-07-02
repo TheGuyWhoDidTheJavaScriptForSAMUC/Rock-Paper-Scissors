ec9bdbb6cf03691f1e3b89acd62b7d9188b8a809
Thirty-Ninene <student10@js-c02mxpvdg083.punahou.edu>
2015-07-01 14:08:22 -1000

    stressful

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -61,13 +61,13 @@ function compare(choice1, choice2) {
 	}
 
 	else if(choice1 === "scissors") {
-		if (choice2 === "rock"){
+		if (choice2 === "rock") {
 			alert("You Lose");
 		}
 	}
 
 	else if(choice1 === "paper") {
-		if (choice2 === "scissors"){
+		if (choice2 === "scissors") {
 			alert("You Lose");
 		}
 	}
