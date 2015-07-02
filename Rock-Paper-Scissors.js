c5618c261a7e89c80ddd41cc87fb8d8b73137f9c
Thirty-Ninene <student10@js-c02mxpvdg083.punahou.edu>
2015-07-01 14:16:39 -1000

    Hey

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -5,7 +5,7 @@ function computerThink() {
 	if(computerChoice === 1) {
 		computerChoice = "rock";
 	}
-	if(computerChoice === 2) {
+	else if(computerChoice === 2) {
 		computerChoice = "paper";
 	}
 	else {
@@ -74,5 +74,6 @@ function compare(choice1, choice2) {
 }
 
 function results() {
-	alert("You chose " + userChoice + ". Computer chose " + computerChoice);	
+	alert("You chose " + userChoice + ". Computer chose " + computerChoice);
+	compare(userChoice, computerChoice);	
 }
